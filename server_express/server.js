fd0dace9a7396afb9072dfb3d749aaf13268351b
Dangocan <danilogc1999@gmail.com>
2020-10-21 21:09:41 -0300

    .

---
+++ server_express/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import socket from 'socket.io';
+import http from 'http';
+import path from 'path';
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+server.listen(process.env.PORT || 8080, () => console.log(`Servidor rodando`));
+
+io.on('connection', (socket) => {
+    console.log('Server has a new connection');
+    socket.on('chat.message', (data) => {
+        console.log('chat.message -->', data);
+        io.emit('chat.message', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('A connection was disconnected');
+    });
+});
